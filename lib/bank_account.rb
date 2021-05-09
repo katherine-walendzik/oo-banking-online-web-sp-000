6dde453186d8c9fe0f932c777ba51940fe4fea97
Katherine Walendzik <katherinewalendzik@gmail.com>
2021-05-09 18:07:14 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,9 +1,13 @@
 class BankAccount
-  attr_accessor :name
+  attr_accessor :name, :balance, :status
   
   def initialize(name)
     self.name = name
     @balance = 1000
+    @status = "open"
+  end
+  
+  def deposit
   end
 
 end
